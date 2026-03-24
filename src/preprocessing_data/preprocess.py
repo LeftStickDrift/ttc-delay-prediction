6344722aa6e7722b7f7cb34594ce801d1a0c0046
Jeryshan Varatheswaran <jeryvara@gmail.com>
2026-03-23 23:10:44 -0400

    Updated preprocessing
    Adding bound encoding and error handling for reading dataset.

--- src/preprocessing_data/preprocess.py
+++ src/preprocessing_data/preprocess.py
@@ -4,7 +4,13 @@ from sklearn import preprocessing
 
 
 def load_dataset(file_path):
-    return pd.read_csv(file_path)
+
+    if (pd.read_csv(file_path) == pd.DataFrame):
+        return pd.read_csv(file_path) # Dataset
+    
+    else:
+        return (pd.DataFrame) #  Empty dataframe
+
 #can add error handling here
 
 
@@ -30,16 +36,23 @@ def transit_code_encoding(df_c):
 
 #(N - 1, S - 2, E - 3, W-4)
 def bound_encoding(df_c):
+    """Create numeric labels for bound direction and return the label."""
+    if(df_c == 'N'):
+        return 1
+    elif(df_c == 'S'):
+        return 2
+    elif(df_c == 'E'):
+        return 3
+    elif(df_c == 'W'):
+        return 4
 
-
-    return df_c 
 
 
 #one-hot encoding of each unique Line
 def line_encoding(df_c):
-
-
-
+    """Create numeric labels for line and return the label."""
+    #We will need to find a way to encode lines, transits, and station using some sort of loop or built-in function
+   
 
     return df_c
 
